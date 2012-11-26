e4ed230ba0c7f5f637d6c1aa33fd52df19baf30c
Zorgie <zorgie@Zorgie-laptop.(none)>
2012-11-26 13:46:20 -0100

    Börjat på grafhantering

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -10,3 +10,22 @@ Graph::Graph(size_t _size)
 Graph::~Graph()
 {
 }
+
+void Graph::addEdge(int from, int to, double weight)
+{
+	if(from < 0 || from >= size || to < 0 || to >= size)
+	{
+		throw std::out_of_range("Edge nodes out of range");
+	}
+	edges[from][to] = weight;	
+}
+
+map<int, double> Graph::getNeighbours(int from)
+{
+	return &(edges[from]);
+}
+
+double Graph::getEdge(int from, int to)
+{
+	return edges[from][to];
+}
--- src/Graph.h
+++ src/Graph.h
@@ -7,8 +7,13 @@ using namespace std;
 class Graph
 {
 public:
+	// Constructors and destructors
 	Graph(size_t _size);
 	~Graph();
+	// Methods
+	void addEdge(int from, int to, double weight);
+	map<int, double> getNeighbours(int from);
+	double getEdge(int from, int to);
 private:
 	// Fields
 	size_t size;
