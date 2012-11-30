9e29d2233daf33e6a930d53af622317876f65a44
Tim Malmström <tim@GORTHAUR.(none)>
2012-11-30 11:35:53 +0100

    dubbellänkad lista

--- src/Solving.cpp
+++ src/Solving.cpp
@@ -7,12 +7,16 @@ Solving::Solving(std::istream& in)
 	int temp;
 	in >> temp;
 	size = temp;
-	coord = new double*[size];
+	map.resize(size);
+	roads.resize(size);
 	for(int i=0; i<size; i++)
 	{
-		coord[i] = new double[2];
-		in >> coord[i][0];
-		in >> coord[i][1];
+		double x;
+	    in >> x;
+		double y;
+	    in >> y;
+		map[i].set(i,x,y);
+		roads[i].resize(size);
 	}
 }
 
@@ -24,8 +28,9 @@ Solving::~Solving(void)
 
 void Solving::solveTSP()
 {
-	route = new int[size];
+	route.resize(size);
 	bool* used = new bool[size];
+
 	for(int i=0; i<size; i++)
 	{
 		route[i] = 0;
@@ -39,29 +44,134 @@ void Solving::solveTSP()
 		int best = -1;
 		for(int j=0; j<size; j++)
 		{
-			if(!used[j] && (best == -1 || dist(route[i-1], j) < dist(route[i-1], best)))
+			if(!used[j] && (best == -1 || distance(route[i-1], j) < distance(route[i-1], best)))
 				best = j;
 		}
 		route[i] = best;
 		used[best] = true;
 	}
+
+	pathStart = new Route(-1,NULL,NULL);
+	Route* path = pathStart;
+	for(int i = 0; i<size;i++)
+	{
+	path->nextr = new Route(route[i],path,NULL);
+	path = path->next(path);
+	map[route[i]].pos = path; 
+	}
+	
+	pathEnd = new Route(-1,path,NULL);
+	(*path).nextr = pathEnd;
+}
+
+void Solving::twoOpt(){
+	Route* tmp = pathStart->next(pathStart);
+	int lastid ;
+	while(tmp->ID()!=-1)
+	{
+		lastid=tmp->ID();
+		tmp = tmp->next(tmp);
+		if(tmp == pathEnd)
+		{
+				std::cout << "what!?!?!?!?" << std::endl;
+		}
+
+		Route* tmp2 = pathStart->next(pathStart);
+			while(tmp2->ID()!=-1)
+			{
+				int lastinnerid=tmp2->ID();
+				tmp2 = tmp2->next(tmp2);
+
+
+				if((distance(lastid,tmp->ID())+distance(lastinnerid,tmp2->ID())>distance(lastid,lastinnerid)+distance(tmp->ID(),tmp2->ID())) && okayCombo(lastid,tmp->ID(),lastinnerid,tmp2->ID()))
+				{
+						switchRoads(lastid,tmp->ID(),lastinnerid,tmp2->ID());
+						return;
+				}
+
+			}
+
+	}
+}
+
+bool Solving::okayCombo(int a, int b, int c, int d)
+{
+		if(a != b && a != c && a != d && b != c && b != d && c != d)
+				return true;
+		return false;
+
+}
+
+void Solving::switchRoads(int A,int B, int C, int D)
+{// switches the road A-B, C-D to A-C, B-D
+		std::cout << A << " "<< B << " "<< C << " "<< D << " " << std::endl;
+Route* a = map[A].pos;
+Route* b = map[B].pos;
+Route* c = map[C].pos;
+Route* d = map[D].pos;
+a->replace(b,c);
+c->replace(d,a);
+b->replace(a,d);
+d->replace(c,b);
+
+}
+
+void Solving::improveTSP()
+{
+		twoOpt();
+	
+
 }
 
 void Solving::printTSP()
 {
+		Route* tmp = pathStart->next(pathStart);
+		while(tmp->ID() != -1)
+		{
+			printf("%d\n",tmp->ID());		
+			tmp = tmp->next(tmp);
+		}
+/*
 	for(int i=0; i<size; i++)
 	{
 		printf("%d\n",route[i]);		
 	}
+	*/
+
+	std::cout << "totalt avstånd: " << totalDistance()  << std::endl;
 }
 
-double Solving::dist(const int& _first, const int& _second) const
+
+int Solving::distance(int c1, int c2)
 {
-	// Euclidian distance
-	double x1, y1, x2, y2;
-	x1 = coord[_first][0];
-	y1 = coord[_first][1];
-	x2 = coord[_second][0];
-	y2 = coord[_second][1];
-	return sqrt(pow(x1-x2, 2) + pow(y1-y2, 2));
+	return distance(map[c1],map[c2]);
+}
+int Solving::distance(const City ID1,const City ID2)
+{
+
+		if(roads[ID1.ID][ID2.ID] != 0)
+		{
+			return roads[ID1.ID][ID2.ID];
+		}
+
+	int tmp = round(sqrt(pow(ID1.coordX-ID2.coordX,2) + pow(ID1.coordY-ID2.coordY,2)));
+
+	roads[ID1.ID][ID2.ID] = tmp;
+	roads[ID2.ID][ID1.ID] = tmp;
+	return tmp;
+}
+int Solving::totalDistance()
+{
+	int sum = 0;
+	Route* tmp = pathStart->next(pathStart);
+	int lastid = tmp->ID();
+	while(tmp->ID()!=-1)
+	{
+		sum += distance(lastid,tmp->ID());
+		lastid=tmp->ID();
+		tmp = tmp->next(tmp);
+		//std::cout << sum << " - " << lastid << tmp->ID() << std::endl;
+	}
+	sum += distance(lastid,pathStart->next(pathStart)->ID());
+	return sum;
 }
--- src/TSP.cpp
+++ src/TSP.cpp
@@ -1,14 +1,19 @@
 // TSP.cpp : Defines the entry point for the console application.
-//
 
 #include <stdio.h>
-#include "Graph.h"
+#include <fstream>
 #include "Solving.h"
 
 int main(int argc, char* argv[])
 {
-	Solving s(std::cin);
+		std::ifstream TSP("TSP.txt");
+	Solving s(TSP);
+	TSP.close();
+
+	//Solving s(std::cin);
 	s.solveTSP();
-	s.printTSP();
+	s.improveTSP(); // när man har en lösning... börja ösa optimeringar
+	//s.printTSP();
 	return 0;
 }
+
