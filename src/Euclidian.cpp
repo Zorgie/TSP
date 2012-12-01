c4314e93ba4542f03fdbe80aec5bf3180fa7a798
Lucas <thezorg@hotmail.com>
2012-12-01 23:35:14 +0100

    2-opt omskriven, 2.5-opt tillagt
    * Tog bort förberäkningar för avstånd
    * Tog bort avståndsmatris
    * Skrev om lagring av stig
    * Skrev om 2-opt
    * Var awesome.
    * 3-opt implementerad, men trasig.

--- src/Euclidian.cpp
+++ src/Euclidian.cpp
@@ -1,148 +1,181 @@
 #include "Euclidian.h"
 
+
 Euclidian::Euclidian(std::istream& in)
 {
+	// Read input
 	int temp;
 	in >> temp;
 	size = temp;
+	// Reads the point data.
 	points = new Point[size];
 	for(int i=0; i<size; i++)
 	{
 		in >> points[i].x;
 		in >> points[i].y;
 	}
-	initialize();
+
+	// Calculates the K closest cities for each blah blah.
+	initDistances();
+}
+
+void Euclidian::initDistances()
+{
+	K = min(K_MAX, (int)size-1);
+	distanceMatrix = new double*[size];
 }
 
 
 Euclidian::~Euclidian(void)
 {
-	//delete[] points;
-	//for(int i=0; i<size; i++)
-	//	delete[] pointsByDistance[i];
-	//delete[] pointsByDistance;
 }
+
 
 void Euclidian::solve()
 {
 	greedy();
-	for(int i=0; i<TWO_OPT_COUNT; i++)
+}
+
+void Euclidian::greedy()
+{
+	solution = new int[size];
+	bool* used = new bool[size];
+	for(int i=0; i<size; i++)
 	{
-		if(!opt2Cycle())
-			break;
+		solution[i] = 0;
+		used[i] = false;
+	}
+
+	solution[0] = 0;
+	used[0] = true;
+	for(int i=1; i<size; i++)
+	{
+		int best = -1;
+		for(int j=0; j<size; j++)
+		{
+			if(!used[j] && (best == -1 || dist(solution[i-1], j) < dist(solution[i-1], best)))
+				best = j;
+		}
+		solution[i] = best;
+		used[best] = true;
 	}
 }
+
+void Euclidian::ruinEverything(int count)
+{
+	if(size < 4)
+		return;
+	for(int i=0; i<count; i++)
+	{
+		int from = rand() % size;
+		int to = from;
+		while(to == from)
+		{
+			to = rand() % size;
+		}
+		reverse(solution, from, to);
+	}
+}
+
+void Euclidian::opt2Cycle(clock_t start, int timeout)
+{
+	if(size < 4)
+		return;
+	while(true){
+		for(int i=0; i<size-3; i++)
+		{
+			time_t now = clock();
+			int time = (((double)now-start)/CLOCKS_PER_SEC*1000);
+			if(time > timeout)
+				return;
+			for(int j=2; j< (size-1); j++)
+			{
+				int to = (i + j)%size;
+				if(swapGood(i, to))
+				{
+					reverse(solution, i+1, to);
+					if(i > 0 && time < timeout * 0.65)
+					{
+						i--;
+					}
+				}
+			}
+		}
+	}
+}
+void Euclidian::opt25Cycle(clock_t start, int timeout)
+{
+	if(size < 4)
+		return ;
+	for(int i=0; i<size; i++)
+	{
+		time_t now = clock();
+		int time = (((double)now-start)/CLOCKS_PER_SEC*1000);
+		if(time > timeout)
+			return ;
+		// i is the node that will be moved.
+		for(int j=1; j<size-1; j++)
+		{
+			if(insertGood(i, j))
+			{
+				insert(solution, i, j);
+			}
+		}
+	}
+}
+
+void Euclidian::opt3Cycle(clock_t start, int timeout)
+{
+	if(size < 6)
+		return;
+	int* tuple = new int[6];
+	for(int i=0; i<size-4; i++)
+	{
+		time_t now = clock();
+		int time = (((double)now-start)/CLOCKS_PER_SEC*1000);
+		if(time > timeout)
+		{
+			delete[] tuple;
+			return ;
+		}
+		tuple[0] = i;
+		tuple[1] = (i+1)%size;
+		for(int j=2; j<size-i-2; j++)
+		{
+			tuple[2] = (i+j)%size;
+			tuple[3] = (i+j+1)%size;
+			for(int k=2; k<size-i-j; k++)
+			{
+				tuple[4] = (i+j+k)%size;
+				tuple[5] = (i+j+k+1)%size;
+				if(tripleSwapGood(tuple))
+				{
+					reverse(solution, tuple[1], tuple[4]);
+					reverse(solution, tuple[1], tuple[2]);
+				}
+			}
+		}
+	}
+	delete [] tuple;
+}
+
 
 void Euclidian::print()
 {
-	int cur = 0;
-	int count = 0;
-	do{
-		printf("%d\n", cur);
-		cur = solution[cur];
-	}while(cur != 0);
-}
-
-int* Euclidian::getSolution()
-{
-	int cur = 0;
-	int count = 0;
-	int* sol = new int[size];
-	do{
-		sol[count++] = cur;
-		cur = solution[cur];
-	}while(cur != 0);
-	return sol;
-}
-
-void Euclidian::initialize()
-{
-	K = min((int)size-1, K_MAX);
-	pointsByDistance = new int*[size];
 	for(int i=0; i<size; i++)
 	{
-		pointsByDistance[i] = new int[K];
-		bool* used = new bool[size];
-		for(int ii = 0; ii<size;ii++){used[ii]=false;}
-		for(int j = 0; j<K; j++)
-		{
-			double minDist = 100000000; // A large number.
-			int closest = -1;
-			for(int ii = 0; ii < size; ii ++)
-			{
-				if(used[ii] || ii == i)
-					continue;
-				double distance = dist(i, ii);
-				if(distance < minDist)
-				{
-					minDist = distance;
-					closest = ii;
-				}
-			}
-			used[closest] = true;
-			pointsByDistance[i][j] = closest;
-		}
-		delete[] used;
+		printf("%d\n", solution[i]);
 	}
 }
 
-bool Euclidian::opt2Cycle()
+long Euclidian::totalDistance()
 {
-	bool swapped = false;
+	long distance = 0;
 	for(int i=0; i<size; i++)
 	{
-		if(opt2Step(i))
-		{
-			swapped = true;
-		}
+		distance += dist(solution[i], solution[(i+1)%size]);
 	}
-	return swapped;
-}
-
-bool Euclidian::opt2Step(int origin)
-{
-	int* tuple = new int[4];
-	tuple[0] = origin;
-	tuple[1] = solution[origin];
-	for(int i=0; i<K; i++)
-	{
-		tuple[2] = pointsByDistance[tuple[1]][i];
-		tuple[3] = backSolution[tuple[2]];
-		if(tuple[1] != tuple[3] && isOpt2Improvement(tuple))
-		{
-			reverse(tuple[1], tuple[3]);
-			solution[tuple[0]]=tuple[3];
-			solution[tuple[1]]=tuple[2];
-			backSolution[tuple[3]] = tuple[0];
-			backSolution[tuple[2]] = tuple[1];
-			return true;
-		}
-	}
-	delete[] tuple;
-	return false;
-}
-
-bool Euclidian::isOpt2Improvement(int* fourTuple)
-{
-	double dist1 = dist(fourTuple[0], fourTuple[1]);
-	double dist2 = dist(fourTuple[2], fourTuple[3]);
-	double dist3 = dist(fourTuple[0], fourTuple[3]);
-	double dist4 = dist(fourTuple[1], fourTuple[2]);
-	return (dist1 + dist2 > dist3 + dist4);
-}
-
-void Euclidian::reverse(int from, int to)
-{
-	if(from == to)
-		return;
-	int next = solution[from];
-	do{
-		int tempNext = solution[next];
-		solution[next] = from;
-		backSolution[from] = next;
-		from = next;
-		next = tempNext;
-	}while(from != to);
+	return distance;
 }
 
 double Euclidian::dist(int first, int second)
@@ -153,34 +186,84 @@ double Euclidian::dist(int first, int second)
 	return d;
 }
 
-void Euclidian::greedy()
+bool Euclidian::swapGood(int from, int to)
 {
-	int* tour = new int[size];
-	bool* used = new bool[size];
-	for(int i=0; i<size; i++)
-	{
-		tour[i] = 0;
-		used[i] = false;
-	}
+	int from1 = (from+1)%size;
+	int to1 = (to+1)%size;
+	if(from % size == to % size)
+		return false;
+	double dist2 = dist(solution[to], solution[to1]);
+	double dist3 = dist(solution[from], solution[to]);
+	if(dist2 < dist3)
+		return false;
+	double dist1 = dist(solution[from], solution[from1]);
+	double dist4 = dist(solution[from1], solution[to1]);
+	return (dist1 + dist2 > dist3 + dist4);
+}
 
-	tour[0] = 0;
-	used[0] = true;
-	for(int i=1; i<size; i++)
+bool Euclidian::insertGood(int from, int to)
+{
+	int from1, from2;
+	int to2;
+	from1 = (from-1+size)%size;
+	from2 = (from+1)%size;
+	to2 = (to+1)%size;
+	double* distance = new double[6];
+	distance[0] = dist(solution[from1], solution[from]);
+	distance[1] = dist(solution[from], solution[from2]);
+	distance[2] = dist(solution[to], solution[to2]);
+	distance[3] = dist(solution[from1], solution[from2]);
+	distance[4] = dist(solution[to], solution[from]);
+	distance[5] = dist(solution[from], solution[to2]);
+	bool whaat = distance[0] + distance[1] + distance[2] > distance[3] + distance[4] + distance[5];
+	delete[] distance;
+	return whaat;
+}
+
+bool Euclidian::tripleSwapGood(int* tuple)
+{
+	double* distance = new double[6];
+	distance[0] = dist(solution[tuple[0]], solution[tuple[1]]);
+	distance[1] = dist(solution[tuple[2]], solution[tuple[3]]);
+	distance[2] = dist(solution[tuple[4]], solution[tuple[5]]);
+
+	distance[3] = dist(solution[tuple[0]], solution[tuple[3]]);
+	distance[4] = dist(solution[tuple[4]], solution[tuple[2]]);
+	distance[5] = dist(solution[tuple[1]], solution[tuple[5]]);
+	
+	bool whaat = distance[0] + distance[1] + distance[2] > distance[3] + distance[4] + distance[5];
+	delete[] distance;
+	return whaat;
+}
+
+void Euclidian::reverse(int* a, int from, int to)
+{
+	if(from > to)
 	{
-		int best = -1;
-		for(int j=0; j<size; j++)
-		{
-			if(!used[j] && (best == -1 || dist(tour[i-1], j) < dist(tour[i-1], best)))
-				best = j;
-		}
-		tour[i] = best;
-		used[best] = true;
+		int temp = from;
+		from = (to+1) % size;
+		//from = to+1;
+		to = (temp-1 + size) % size;
+		//to = temp -1;
 	}
-	for(int i=0; i<size; i++)
+	while(from < to)
 	{
-		int cur = tour[i];
-		int next = tour[(i+1)%size];
-		solution[cur]=next;
-		backSolution[next] = cur;
+		int temp = a[from];
+		a[from] = a[to];
+		a[to] = temp;
+		from++;
+		to--;
 	}
+}
+
+void Euclidian::insert(int* a, int from, int to)
+{
+	int temp = a[from];
+	while(from != to)
+	{
+		int next = (from + 1)%size;
+		a[from] = a[next];
+		from = next;
+	}
+	a[from] = temp;
 }
